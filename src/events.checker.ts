d3ff2bc278ddb64505f63c6d6968e514e3f9e84c
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-21 19:28:26 +0300

    feat: add filter log functionality

--- src/events.checker.ts
+++ src/events.checker.ts
@@ -1,16 +1,22 @@
-import { Finding, HandleTransaction, TransactionEvent } from "forta-agent";
+import { Finding, HandleTransaction, Log, TransactionEvent } from "forta-agent";
 import { FindingGenerator } from "./utils";
 
 export default function provideEventCheckerHandler(
   createFinding: FindingGenerator,
   eventSignature: string,
-  address?: string
+  address?: string,
+  filter?: (log: Log, index?: number, array?: Log[]) => boolean
 ): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
 
-    if (txEvent.filterEvent(eventSignature, address).length > 0) {
-      findings.push(createFinding());
+    if (filter) {
+      txEvent
+        .filterEvent(eventSignature, address)
+        .filter(filter)
+        .map(() => findings.push(createFinding()));
+    } else {
+      txEvent.filterEvent(eventSignature, address).map(() => findings.push(createFinding()));
     }
 
     return findings;
