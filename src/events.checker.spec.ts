d73453f71b15d63fe05fc2d9c6bd26484bd62b89
Lázaro Raúl <riglesiasvera@gmail.com>
2021-09-29 09:56:07 -0400

    feat: Add test for testing the findings metadata

--- src/events.checker.spec.ts
+++ src/events.checker.spec.ts
@@ -1,8 +1,24 @@
-import { HandleTransaction, Finding, Transaction, TransactionEvent, Log } from "forta-agent";
+import { HandleTransaction, Finding, TransactionEvent, Log, FindingSeverity, FindingType } from "forta-agent";
 import { generalTestFindingGenerator, TestTransactionEvent } from "./tests.utils";
 import provideEventCheckerHandler from "./events.checker";
+import { FindingGenerator, metadataVault } from "./utils";
+import { keccak256 } from "forta-agent/dist/sdk/utils";
 
 const EVENT_SIGNATURE = "testSignature(bool,address)";
+
+const findingGenerator: FindingGenerator = (event?: metadataVault): Finding =>
+  Finding.fromObject({
+    name: "Finding Test",
+    description: "Finding for test",
+    alertId: "TEST",
+    severity: FindingSeverity.Low,
+    type: FindingType.Unknown,
+    metadata: {
+      topics: JSON.stringify(event?.topics),
+      data: event?.data,
+      address: event?.address,
+    },
+  });
 
 describe("Event Checker Agent Tests", () => {
   let transactionHandler: HandleTransaction;
@@ -112,5 +128,71 @@ describe("Event Checker Agent Tests", () => {
     const findings: Finding[] = await transactionHandler(txEvent);
 
     expect(findings).toStrictEqual([generalTestFindingGenerator(txEvent)]);
+  });
+
+  it("should returns findings with metadata if condition met", async () => {
+    const filterLog = (log: Log): boolean => {
+      const number = Number(BigInt(log.data)) / 10 ** 18;
+      if (number > 2) {
+        return true;
+      }
+
+      return false;
+    };
+
+    const address: string = "0x121212";
+    const topics: string[] = ["0x10", "0x20", "0x30"];
+    const data: string = "0x00000000000000000000000000000000000000000000000029a2241af62c0000"; // 3
+
+    transactionHandler = provideEventCheckerHandler(
+      findingGenerator,
+      EVENT_SIGNATURE,
+      address,
+      filterLog
+    );
+
+    const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
+      EVENT_SIGNATURE,
+      address,
+      topics,
+      data,
+    );
+    const findings: Finding[] = await transactionHandler(txEvent);
+
+    expect(findings).toStrictEqual([
+      findingGenerator({
+        topics: [keccak256(EVENT_SIGNATURE), ...topics], 
+        address: address, 
+        data: data,
+      }),
+    ]);
+  });
+
+  it("should returns findings with metadata if the event was emitted", async () => {
+    const address: string = "0x121212A";
+    const topics: string[] = ["0xA", "0xB", "0xC"];
+    const data: string = "0x00000000000000000000000000000000000000000000000029a2241af62c0000";
+
+    transactionHandler = provideEventCheckerHandler(
+      findingGenerator,
+      EVENT_SIGNATURE,
+      address,
+    );
+
+    const txEvent: TransactionEvent = new TestTransactionEvent().addEventLog(
+      EVENT_SIGNATURE,
+      address,
+      topics,
+      data,
+    );
+    const findings: Finding[] = await transactionHandler(txEvent);
+
+    expect(findings).toStrictEqual([
+      findingGenerator({
+        topics: [keccak256(EVENT_SIGNATURE), ...topics], 
+        address: address, 
+        data: data,
+      }),
+    ]);
   });
 });
