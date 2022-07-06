104c586e458e4493aa63eb7f985721dc61e8601c
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-07-06 17:15:18 -0300

    refac: Add return type of CachedProvider.from()

--- src/utils/cached_provider.ts
+++ src/utils/cached_provider.ts
@@ -8,7 +8,7 @@ export class CachedProvider {
   static cache = new LRU<string, Promise<string>>({ max: 1000 });
   static mutex = new Mutex();
 
-  public static from(provider: ethers.providers.Provider, cacheByBlockTag: boolean = true) {
+  public static from(provider: ethers.providers.Provider, cacheByBlockTag: boolean = true): ethers.providers.Provider {
     return new Proxy(provider, {
       get(target: ethers.providers.Provider, prop: keyof ethers.providers.Provider) {
         if (!(prop in target)) throw new Error(`Unknown provider property: ${prop}`);
